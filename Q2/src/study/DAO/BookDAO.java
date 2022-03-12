476dc51e367f0d924eb6237e36ab499930a20196
danny <mirkute151296@gmail.com>
2022-03-12 12:07:00 +0530

    package delete

--- Q2/src/study/DAO/BookDAO.java
+++ Q2/src/study/DAO/BookDAO.java
@@ -1,12 +1,11 @@
-package study.DAO;
+
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
-import study.entity.Book;
+
 
 public class BookDAO {
 	
